78701b31e4e86e957ed8f3d9d050b2164cd26fd3
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-09-29 10:39:57 +0600

    load random image from picsum

--- app/src/main/java/com/example/introbanglatask/MainActivity.java
+++ app/src/main/java/com/example/introbanglatask/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.introbanglatask;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,32 +14,52 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    ImageView randomImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView randomImageView=findViewById(R.id.randomImageView);
+        randomImageView=findViewById(R.id.randomImageView);
         Button loadButton=findViewById(R.id.loadImageButton);
         TextView textView=findViewById(R.id.textView);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(networkAvailability()){
                     textView.setVisibility(View.GONE);
+                    loadRandomImage();
                 }else{
                     textView.setVisibility(View.VISIBLE);
-                    textView.setText("No network. Please check connection");
+                    textView.setText("No network.Please check your connection");
                 }
             }
         });
 
-
+    }
+    private void loadRandomImage(){
+        try {
+            URL url=new URL("https://picsum.photos/300/300");
+            HttpURLConnection httpURLConnection= (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+            InputStream inputStream=httpURLConnection.getInputStream();
+            Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
+            randomImageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private boolean networkAvailability() {
